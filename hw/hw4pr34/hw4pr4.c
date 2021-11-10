dc52bacf36340f22d0a8a200dc7ea79bc1321102
Gary Kim <gary@garykim.dev>
2021-11-10 15:22:03 -0500

    hw4pr4: add multiple signals sent by each child
    Signed-off-by: Gary Kim <gary@garykim.dev>

--- hw/hw4pr34/hw4pr4.c
+++ hw/hw4pr34/hw4pr4.c
@@ -13,11 +13,11 @@ const char* help_message =
     "%1$s - try signals\n"
     "\n"
     "USAGE:\n"
-    "\t%1$s signal_number signal_sends\n";
+    "\t%1$s signal_number killer_children signal_sends\n";
 
 int sig_count = 0;
 void signal_handler();
-void create_processes(int n, int sig);
+void create_processes(int childnum, int n, int sig);
 int main(int argc, char** argv){
     struct sigaction sa;
     sa.sa_handler=signal_handler;
@@ -26,20 +26,21 @@ int main(int argc, char** argv){
     int signal;
     int n;
 
-    if(argc < 3) {
+    if(argc < 4) {
       fprintf(stderr, help_message, argv[0]);
       return 0;
     }
 
     //get the signal need to be handled from args
     signal = (int)strtol(argv[1], NULL, 10);
-    n = (int)strtol(argv[2], NULL, 10);
+    int children = (int)strtol(argv[2], NULL, 10);
+    n = (int)strtol(argv[3], NULL, 10);
     if(sigaction(signal, &sa, 0)==-1){
         fprintf(stderr, "error in sigaction: %s\n", strerror(errno));
         return -1;
     }
-    create_processes(n, signal);
-    printf("Made %i signals received %i signals.\n", n, sig_count);
+    create_processes(children, n, signal);
+    printf("Made %i signals from %i children sending %i signals each, received %i signals.\n", n * children, children, n, sig_count);
 }
 
 void signal_handler(){
@@ -47,10 +48,10 @@ void signal_handler(){
     return;
 }
 
-void create_processes(int n, int sig){
+void create_processes(int childnum, int n, int sig){
     pid_t pid = getpid();
-    pid_t* children = alloca(n * sizeof(pid_t));
-    for(int i=0;i<n;i++){
+    pid_t* children = alloca(childnum * sizeof(pid_t));
+    for(int i=0;i<childnum;i++){
         int child = fork();
         switch(child){
             case -1:
@@ -58,8 +59,10 @@ void create_processes(int n, int sig){
                 exit(1);
                 break;
             case 0: 
-                if(kill(pid, sig)==-1){
-                    exit(errno);
+                for (int j = 0; j < n; j++) {
+                    if(kill(pid, sig)==-1){
+                        exit(errno);
+                    }
                 }
                 exit(0);
                 break;
@@ -68,7 +71,7 @@ void create_processes(int n, int sig){
                 break;
         }
     }
-    for(int i=0;i<n;i++){
+    for(int i=0;i<childnum;i++){
         int wstatus;
         if (waitpid(children[i],&wstatus,0) != children[i]) {
           fprintf(stderr, "error: unexpected return waiting for child process: %s", strerror(wstatus));
@@ -83,3 +86,4 @@ void create_processes(int n, int sig){
         }
     }
 }
+
